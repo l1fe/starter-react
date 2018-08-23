54e0d200dcea1b93388bf9af9154e9e1cd069b53
Igor Naumov <igor.naumov.17@gmail.com>
2018-08-23 14:53:29 +0300

    Setup react-router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import configureStore from 'redux/store/configureStore';
+import { Home } from 'containers';
 import 'styles/base.scss';
 
 const preloadedState = { };
@@ -9,9 +11,12 @@ const store = configureStore(preloadedState);
 
 const App = () => (
   <Provider store={store}>
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Hello, React</h1>
+        <Route exact path="/" component={Home} />
+      </div>
+    </BrowserRouter>
   </Provider>
 );
 
