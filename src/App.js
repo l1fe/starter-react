96793332754371200f2b5cc5445c5362cc61caa1
Igor Naumov <igor.naumov.17@gmail.com>
2018-08-23 12:05:17 +0300

    Enabled CSS Modules, added SASS loader, configured 'styles' folder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 
-import LogoIconSrc from 'assets/svg/logo.svg';
-import './App.css';
+import 'styles/base.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={LogoIconSrc} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Hello, React!</h1>
       </div>
     );
   }
